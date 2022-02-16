59ed894bb8988b495d0960819c922fef7a825a48
andre <andreconghau@gmail.com>
2022-02-16 10:39:01 +0700

    todolist

---
+++ my-app/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoList() {
+    return (
+        <div className={"todo-list-wrapper"}>
+            <p>here</p>
+        </div>
+    )
+}
+
+export default TodoList;
